4f463c58a9ddba1215cf5f744dc6fdd54060cdcd
Kevin Xu <kevinxu820@gmail.com>
2020-12-30 22:43:24 -0500

    First commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,30 @@
+from flask import Flask, request, redirect, render_template
+from werkzeug.utils import secure_filename
 import time
-from flask import Flask
+import csv
 
 app = Flask(__name__)
 
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+	daterange_from = request.form.get('daterange_from')
+	daterange_to = request.form.get('daterange_to')
+	f = request.files['myfile']
+	f.save(secure_filename(f.filename))
+
+	with open(f.filename) as csv_file:
+		reader = csv.reader(csv_file)
+		for row in reader:
+			print(row)
+	
+	return '''<h1>Date starts at: {}</h1>
+              <h1>Date ends at: {}</h1>
+              <h1>File name is: {}</h1>'''.format(daterange_from, daterange_to, f.filename)
+
 @app.route('/time')
 def get_current_time():
-	return {'time': time.time()}
+    return {'time': time.time()}
+
+if __name__ == "__main__":
+    app.run()
--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+//import DateComponent from "./DateComponent"
+import logo from "./sts-logo.png";
+import React, { useState, useEffect } from 'react';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker/dist/entry';
 
-function App() {
+
+function App(){
+  const [value, onChange] = useState([new Date(), new Date()]);
+  //const [currentTime, setCurrentTime] = useState(0);
+
+  // useEffect(() => {
+  //   fetch('/time').then(res => res.json()).then(data => {
+  //     setCurrentTime(data.time);
+  //   });
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" className="App-logo"/>
+        <form action="http://localhost:5000/result" method="post" enctype = "multipart/form-data">
+          <label for="myfile" className="file-upload">Select a file:</label>
+          <input type="file" id="myfile" name="myfile" /> <br /> <br /> 
+          
+          <div>
+            <DateRangePicker
+              onChange={onChange}
+              value={value}
+            />
+          </div>
+          
+          {/* <div>
+            <DateComponent />
+          </div> */}
+          <br />
+          <input type="submit" className="submit-button" value="Submit"/>
+        </form>
       </header>
     </div>
   );
