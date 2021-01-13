0c3abe4dd1040f1ecb0065be9a0841de83e48fe7
Kevin Xu <kevinxu820@gmail.com>
2021-01-12 22:18:35 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -84,8 +84,6 @@ def result():
                     print(df_.index.date[i], df_.iloc[i]["Buy_Signal_Price"], df_.iloc[i]["Sell_Signal_Price"])
                     data.append([df_.index.date[i], df_.iloc[i]["Buy_Signal_Price"], df_.iloc[i]["Sell_Signal_Price"]])
             
-            print(data)
-            
             good_data = []
             for i in range(len(data)):
                 if i != len(data) - 1 and not math.isnan(data[i][1]):
@@ -96,9 +94,6 @@ def result():
                     pair.append(change)
                     pair.append("Long")
                     good_data.append(pair)
-                    print(pair)
-            
-            print(good_data)
             
             record_df = pd.DataFrame(good_data, 
                   columns =['Entry_Date', 'Entry_Price', 'Exit_Price', 'Exit_Date', "Percentage_Change", "Long/Short"]) 
