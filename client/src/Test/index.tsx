ae2aeac2a870438861a254c39c04b8a8b71c7bdc
MaxCo919 <yuchenwang919@gmail.com>
2021-01-20 23:41:47 -0500

    add formdata

--- client/src/Test/index.tsx
+++ client/src/Test/index.tsx
@@ -1,18 +1,41 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
+type submitDataType = {
+  underlying: string;
+  expMonth: string;
+  method: string;
+  startDate: string;
+  endDate: string;
+  file: any;
+};
 
 const Test: FunctionComponent<any> = (props) => {
+  const emptyData: submitDataType = {
+    underlying: "",
+    expMonth: "",
+    method: "",
+    startDate: "",
+    endDate: "",
+    file: "",
+  };
   const [validatedForm, setValidatedForm] = useState<boolean>(false);
   const [monthCheck, setMonthCheck] = useState<boolean>(false);
+  const [formData, setFormData] = useState<submitDataType>(emptyData);
 
   const handleSubmit = (e: any) => {
     const form = e.currentTarget;
+    e.preventDefault();
     if (form.checkValidity() === false) {
-      e.preventDefault();
       e.stopPropagation();
+      setValidatedForm(true);
+      setTimeout(() => {
+        setValidatedForm(false);
+      }, 3000);
+    }else{
+      console.log(formData);
     }
-
-    setValidatedForm(true);
   };
 
   return (
@@ -22,6 +45,9 @@ const Test: FunctionComponent<any> = (props) => {
         <Form.Control
           type="text"
           placeholder="Please enter underlying here"
+          onChange={(e: any) => {
+            setFormData({ ...formData, underlying: e.target.value });
+          }}
           required
         />
         <Form.Control.Feedback type="invalid">
@@ -36,22 +62,30 @@ const Test: FunctionComponent<any> = (props) => {
           required
           isInvalid={monthCheck}
           onChange={(e: any) => {
-            if (e.target.value > 99) {
+            if (e.target.value > 99 || e.target.value === null) {
               setMonthCheck(true);
             } else {
               setMonthCheck(false);
             }
+            setFormData({ ...formData, expMonth: e.target.value });
           }}
         />
         <Form.Control.Feedback type="invalid">
-          Expiration Month only allows 2 numbers.
+          This field can't be empty. And you must enter no more than 2 numbers.
         </Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="method">
         <Form.Label>Select a method:</Form.Label>
-        <Form.Control as="select" placeholder="Select a value:" required>
+        <Form.Control
+          as="select"
+          placeholder="Select a value:"
+          required
+          onChange={(e: any) => {
+            setFormData({ ...formData, method: e.target.value });
+          }}
+        >
           <option>First Of Month</option>
-          <option>Last Of Month</option>
+          <option>Last Trading Day</option>
           <Form.Control.Feedback type="invalid">
             You must select a value.
           </Form.Control.Feedback>
@@ -60,27 +94,37 @@ const Test: FunctionComponent<any> = (props) => {
       <Row>
         <Col>
           <Form.Group controlId="startdate">
-            <Form.Label>Select date:</Form.Label>
+            <Form.Label>Select Start date:</Form.Label>
             <Form.Control
               type="date"
-              placeholder="Select a value:"
+              placeholder="Select start date:"
+              onChange={(e: any) => {
+                setFormData({ ...formData, startDate: e.target.value });
+              }}
               required
             ></Form.Control>
           </Form.Group>
         </Col>
         <Col>
           <Form.Group controlId="enddate">
-            <Form.Label>Select date:</Form.Label>
+            <Form.Label>Select End date:</Form.Label>
             <Form.Control
               type="date"
-              placeholder="Select a value:"
+              placeholder="Select end date:"
+              onChange={(e: any) => {
+                setFormData({ ...formData, endDate: e.target.value });
+              }}
               required
             ></Form.Control>
           </Form.Group>
         </Col>
       </Row>
       <Form.Group controlId="files">
-        <Form.File label="Select your file here" custom />
+        <Form.File label="Select your file here"
+         onChange={(e: any) => {
+          setFormData({ ...formData, file: e.target.files });
+        }}
+        />    
       </Form.Group>
       <Button type="submit">Submit form</Button>
     </Form>
