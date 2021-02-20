016b97cad23e417ea0425a59dc9b2b96b2d9de00
MaxCo919 <yuchenwang919@gmail.com>
2021-02-19 20:01:56 -0500

    add button to hide input form

--- client/src/Test/index.tsx
+++ client/src/Test/index.tsx
@@ -24,6 +24,7 @@ const Test: FunctionComponent<any> = (props) => {
   const [formData, setFormData] = useState<submitDataType>(emptyData);
   const [tableHeader, setTableHeader] = useState<any>(<></>);
   const [tableRows, setTableRows] = useState<any>(<></>);
+  const [showbutton, setShowButton] = useState<boolean>(true);
 
   const handleSubmit = (e: any) => {
     const form = e.currentTarget;
@@ -42,6 +43,7 @@ const Test: FunctionComponent<any> = (props) => {
         params: formData,
       })
         .then((response) => {
+          setShowButton(!showbutton);
           console.log(response.data);
           let tempHeader = Object.keys(response.data).map((d) => <th>{d}</th>);
           let tempRows = Object.keys(response.data).map((d) => (
@@ -57,95 +59,110 @@ const Test: FunctionComponent<any> = (props) => {
   };
   return (
     <div>
-      <Form noValidate validated={validatedForm} onSubmit={handleSubmit}>
-        <Form.Group controlId="underlying">
-          <Form.Label>Underlying:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Please enter underlying here"
-            onChange={(e: any) => {
-              setFormData({ ...formData, underlying: e.target.value });
-            }}
-            value={formData.underlying}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Underlying can't be empty.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="expiremonth">
-          <Form.Label>Expiration Month:</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Please enter expiration month here"
-            required
-            isInvalid={monthCheck}
-            value={formData.expMonth}
-            onChange={(e: any) => {
-              if (e.target.value > 99 || e.target.value === null) {
-                setMonthCheck(true);
-              } else {
-                setMonthCheck(false);
-              }
-              setFormData({ ...formData, expMonth: e.target.value });
-            }}
-          />
-          <Form.Control.Feedback type="invalid">
-            This field can't be empty. And you must enter no more than 2
-            numbers.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="methods">
-          <Form.Label>Select a method:</Form.Label>
-          <Form.Control
-            as="select"
-            placeholder="Select a value:"
-            onChange={(e: any) => {
-              setFormData({ ...formData, methods: e.target.value });
-            }}
-            value={formData.methods}
-            required
-          >
-            <option></option>
-            <option>First Of Month</option>
-            <option>Last Trading Day</option>
+      <Button
+        variant="outline-primary"
+        onClick={() => {
+          setShowButton(!showbutton);
+        }}
+      >
+        {showbutton ? "Hide Input Form" : "Show Input Form"}
+      </Button>
+      {showbutton ? (
+        <Form noValidate validated={validatedForm} onSubmit={handleSubmit}>
+          <Form.Group controlId="underlying">
+            <Form.Label>Underlying:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Please enter underlying here"
+              onChange={(e: any) => {
+                setFormData({ ...formData, underlying: e.target.value });
+              }}
+              value={formData.underlying}
+              required
+            />
             <Form.Control.Feedback type="invalid">
-              You must select a value.
+              Underlying can't be empty.
             </Form.Control.Feedback>
-          </Form.Control>
-        </Form.Group>
-        <Row>
-          <Col>
-            <Form.Group controlId="startdate">
-              <Form.Label>Select Start Date:</Form.Label>
-              <Form.Control
-                type="date"
-                placeholder="Select start date:"
-                onChange={(e: any) => {
-                  setFormData({ ...formData, daterange_from: e.target.value });
-                }}
-                value={formData.daterange_from}
-                required
-              ></Form.Control>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="enddate">
-              <Form.Label>Select End Date:</Form.Label>
-              <Form.Control
-                type="date"
-                placeholder="Select end date:"
-                onChange={(e: any) => {
-                  setFormData({ ...formData, daterange_to: e.target.value });
-                }}
-                value={formData.daterange_to}
-                required
-              ></Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-        <Button type="submit">Submit form</Button>
-      </Form>
+          </Form.Group>
+          <Form.Group controlId="expiremonth">
+            <Form.Label>Expiration Month:</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Please enter expiration month here"
+              required
+              isInvalid={monthCheck}
+              value={formData.expMonth}
+              onChange={(e: any) => {
+                if (e.target.value > 99 || e.target.value === null) {
+                  setMonthCheck(true);
+                } else {
+                  setMonthCheck(false);
+                }
+                setFormData({ ...formData, expMonth: e.target.value });
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              This field can't be empty. And you must enter no more than 2
+              numbers.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="methods">
+            <Form.Label>Select a method:</Form.Label>
+            <Form.Control
+              as="select"
+              placeholder="Select a value:"
+              onChange={(e: any) => {
+                setFormData({ ...formData, methods: e.target.value });
+              }}
+              value={formData.methods}
+              required
+            >
+              <option></option>
+              <option>First Of Month</option>
+              <option>Last Trading Day</option>
+              <Form.Control.Feedback type="invalid">
+                You must select a value.
+              </Form.Control.Feedback>
+            </Form.Control>
+          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group controlId="startdate">
+                <Form.Label>Select Start Date:</Form.Label>
+                <Form.Control
+                  type="date"
+                  placeholder="Select start date:"
+                  onChange={(e: any) => {
+                    setFormData({
+                      ...formData,
+                      daterange_from: e.target.value,
+                    });
+                  }}
+                  value={formData.daterange_from}
+                  required
+                ></Form.Control>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="enddate">
+                <Form.Label>Select End Date:</Form.Label>
+                <Form.Control
+                  type="date"
+                  placeholder="Select end date:"
+                  onChange={(e: any) => {
+                    setFormData({ ...formData, daterange_to: e.target.value });
+                  }}
+                  value={formData.daterange_to}
+                  required
+                ></Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Button type="submit">Submit form</Button>
+        </Form>
+      ) : (
+        <></>
+      )}
       <Table striped bordered hover>
         <thead>
           <tr>{tableHeader}</tr>
