48d401d355ddfb7062b186283849467717bbc167
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2021-03-14 12:29:52 -0400

    add simulation page

---
+++ client/src/Home/tabs/Simulation.tsx
@@ -0,0 +1,13 @@
+import React,{ FunctionComponent,useEffect, useState,Component} from 'react';
+
+class Simulation extends Component{
+    render(){
+        return(
+            <div>
+                test
+            </div>
+        )
+    }
+}
+
+export default Simulation;
