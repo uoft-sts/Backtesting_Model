f24f6baf366cd7498628d8a45d48c6cb968f0163
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2021-03-20 00:54:19 -0400

    add the tradeview chart

--- client/src/Home/tabs/Simulation.tsx
+++ client/src/Home/tabs/Simulation.tsx
@@ -1,12 +1,16 @@
 import React,{ FunctionComponent,useEffect, useState,Component} from 'react';
+import ChartNew from './ChartNew';
 
 class Simulation extends Component{
+    
     render(){
+        
+        
         return(
-            <div>
-                test
-            </div>
+            <ChartNew />
         )
+
+        
     }
 }
 
