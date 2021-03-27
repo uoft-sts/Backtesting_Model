269c2b752f78647cd71d0166ae53230a82aa3664
Leo6Leo <36619969+Leo6Leo@users.noreply.github.com>
2021-03-26 21:36:45 -0400

    add the date input and the button

--- client/src/Home/tabs/Simulation.tsx
+++ client/src/Home/tabs/Simulation.tsx
@@ -1,13 +1,28 @@
 import React,{ FunctionComponent,useEffect, useState,Component} from 'react';
 import ChartNew from './ChartNew';
-
+import { Form, Button, Row, Col, Table, Image, DropdownButton, Dropdown } from "react-bootstrap";
 class Simulation extends Component{
     
     render(){
         
         
         return(
+
+            
+            <div>
+            <Form.Group controlId="enddate">
+                <Form.Label>Select End Date:</Form.Label>
+                <Form.Control
+                  type="date"
+                  placeholder="Select end date:"
+                  required
+                ></Form.Control>
+              </Form.Group>
+          
+          <Button type="submit" className="btn-block mt-2">Submit form</Button>
+        
             <ChartNew />
+            </div>
         )
 
         
