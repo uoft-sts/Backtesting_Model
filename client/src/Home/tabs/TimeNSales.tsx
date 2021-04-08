e099589beb92478e453732594a3e62a3838d8963
jonathanween <wenyukang0362@gmail.com>
2021-04-08 14:51:39 -0400

    merge 2 table

--- client/src/Home/tabs/TimeNSales.tsx
+++ client/src/Home/tabs/TimeNSales.tsx
@@ -4,8 +4,9 @@ import { Form, Button, Row, Col, Table, Image, DropdownButton, Dropdown, Contain
 
 class TimeNSales extends Component{
 
-    createTable = () => {
-        var my_data = [[10,11,12,13,14,15,17,18],[1,2,3,1,2,3,1,2],[1,2,3,1,2,3,1,2],[10,11,12,13,14,15,17,18],[10,11,12,13,14,15,17,18]]
+    createTableLeft = () => {
+        var my_data = [["PSEc",1184.9,1,"07:58:11"],["PSEc",1184.9,1,"07:58:11"],["PSEc",1184.9,1,"07:58:11"],
+                        ["PSEc",1184.9,1,"07:58:11"],["PSEc",1184.9,1,"07:58:11"],["PSEc",1184.9,1,"07:58:11"]]
         let table = []
         var colour = ["table-primary","table-secondary","table-success", "table-danger"]
         // Outer loop to create parent
@@ -14,7 +15,7 @@ class TimeNSales extends Component{
           let children = []
           //Inner loop to create children
           for (let j = 0; j < my_data[1].length; j++) {
-            children.push(<td>{`hello ${my_data[i][j]}`}</td>)
+            children.push(<td>{`${my_data[i][j]}`}</td>)
           }
           //Create the parent and add the children
           table.push(<tr className={colour[k]}>{children}</tr>)
@@ -26,6 +27,30 @@ class TimeNSales extends Component{
         return table
     }
 
+    createTableRight = () => {
+      var my_data = [["PSEc","1186.0",6,"07:58:20"],["PSEc","1186.0",6,"07:58:20"],
+                      ["PSEc","1186.0",6,"07:58:20"],["PSEc","1186.0",6,"07:58:20"],
+                      ["PSEc","1186.0",6,"07:58:20"],["PSEc","1186.0",6,"07:58:20"]]
+      let table = []
+      var colour = ["table-primary","table-secondary","table-success", "table-danger"]
+      // Outer loop to create parent
+      var k=0
+      for (let i = 0; i < my_data.length; i++) {
+        let children = []
+        //Inner loop to create children
+        for (let j = 0; j < my_data[1].length; j++) {
+          children.push(<td>{`${my_data[i][j]}`}</td>)
+        }
+        //Create the parent and add the children
+        table.push(<tr className={colour[k]}>{children}</tr>)
+        k+=1
+        if (k == 4) {
+          k = 0
+        }
+      }
+      return table
+  }
+
     
     createUpperTable = () => {
       var my_data_second = [10,11,12,13,14,15]
@@ -36,7 +61,7 @@ class TimeNSales extends Component{
         //Inner loop to create children
         // children.push(<td >{`hello ${my_data[i][j]}`}</td>)
         children.push(<td>{`Bid ${my_data_second[0]}`}</td>)
-        children.push(<td>{`Bid Size${my_data_second[1]}`}</td>)
+        children.push(<td>{`Bid Size ${my_data_second[1]}`}</td>)
         children.push(<td>{`Low ${my_data_second[2]}`}</td>)
         children.push(<td>{`change`}</td>)
         //Create the parent and add the children
@@ -70,10 +95,11 @@ class TimeNSales extends Component{
 
                 <Table striped bordered hover className="table table-lg">
     <tbody>
+
         {this.createUpperTable()}  
 
           <tr>
-            <td colSpan={4}>
+            <td colSpan={2}>
             <Table striped bordered hover>
     <thead>
         <tr>
@@ -81,16 +107,33 @@ class TimeNSales extends Component{
         <th>Bid</th>
         <th>Size</th>
         <th>Time</th>
+        </tr>
+    </thead>
+    <tbody>
+        {this.createTableLeft()}  
+    </tbody>
+    </Table>
+            </td>
+
+            <td colSpan={2}>
+
+
+    <Table striped bordered hover>
+    <thead>
+        <tr>
         <th>MM</th>
         <th>Bid</th>
         <th>Size</th>
         <th>Time</th>
         </tr>
+
+
     </thead>
     <tbody>
-        {this.createTable()}  
+        {this.createTableRight()}  
     </tbody>
     </Table>
+
             </td>
           </tr>
 
